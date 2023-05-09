3c60823547ba933af52e03f745ef544dd308a4af
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-05-09 18:51:07 +0200

    Display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@chakra-ui/card";
 import { Game } from "../hooks/useGames";
-import { CardBody, Heading, Image } from "@chakra-ui/react";
+import { CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
