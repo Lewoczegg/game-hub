5d1ee49a57e5b1f1d82d0fc8ae4eafe7eaa8f2a9
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-06-04 15:58:40 +0200

    Refactor - move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card } from "@chakra-ui/card";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
